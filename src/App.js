20af5cdc069e390becdf11b214fe1c1a5272ba7d
m4goth <matheusdealmeidasilveira@gmail.com>
2022-07-10 19:00:33 -0300

    commit inicial, criado projeto

--- src/App.js
+++ src/App.js
@@ -1,4 +1,44 @@
-import logo from './logo.svg';
+// comunicação com api
+import React,{ Component } from 'react';
+import api from './api';
+
+class App extends Component {
+  state= {
+    products: [],
+  }
+
+  async componentDidMount() {
+    // valor do objeto ali nos parenteses logo após api.get
+    const response = await api.get(''); 
+
+    this.setState({ products: response.data});
+  }
+
+  render() {
+
+    const { products } = this.state;
+
+    return (
+       <div>
+          <h1>salve</h1>
+          {products.map(products => (
+            <li key={products.show.id}>
+              <h2>
+                <strong>titulo: </strong>
+                {products.show.name}
+              </h2>
+              <p>
+                {products.show.url}
+              </p>
+            </li>
+          )}
+       </div>
+    );
+  };
+};
+
+
+{/* import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -22,4 +62,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; */}
---
+++ src/api.js
@@ -0,0 +1,8 @@
+// comunicação com api
+import axios from "axios";
+
+    const api = axios.create({
+        baseURL: "https://fakestoreapi.com/products/",
+    });    
+
+export default api;
